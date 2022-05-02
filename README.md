788350ba16ccb85584c9feb7fe14e151072043c2
ciaociaoworld <yellowzung@gmail.com>
2022-05-02 16:08:44 +1000

    project

--- README.md
+++ README.md
@@ -4,10 +4,39 @@
 <h2>Summary of Project</h2>
 
 <h2>Plan</h2>
+
 Step1: What I need to do at first stage
 
-
 1. Think about the basic elements and bonus elements I need to add to this project.
+
+    1.1 Switch Turns
+    use odd and even method to switch the player
+
+    1.2 Design logic for winning & visually display which player won
+        
+        1.2.1 use sample code blow:
+```
+var parentBox = document.querySelector('section')
+
+parentBox.addEventListener('click', function(event) {
+    if(event.target.tagName === 'DIV') {
+        event.target.className = 'red'
+        //you need to create a Class and backroundColor is red
+    }
+    // below is becasue querySelectorAll is like array. so need to use for loop to measure if 3 boxes turned to red.
+    if(document.querySelectorAll('.red').length === 3) {
+        var allDivElements = document.querySelectorAll('div')
+        for(let i=0; i < allDivElements.length; i++) {
+            allDivElements[i].className = 'green'
+        }
+    }
+})
+```
+        
+        1.2.2 As there are 9 boxes. Need to add this logic to each group of 3 boxes.
+
+        Note: Think about boxes contain boxes. array[i][i]
+
 2. Find out which codes I may need to use and which codes are used to add these elements.
 3. Find out the difficult part I may face. Complete Flow Chart at first.
 4. Image the final version of this project, think about what else functions should to be added.
--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 
-// 
-var winningSituation = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [6, 4, 2]
-]
-// player 1 and player 2 
-var player1 = 'O'
-var player2 = 'X'
+var box0 = document.querySelector('#top0')
+var box1 = document.querySelector('#top1')
+var box2 = document.querySelector('#top2')
+var box3 = document.querySelector('#mid3')
+var box4 = document.querySelector('#mid4')
+var box5 = document.querySelector('#mid5')
+var box6 = document.querySelector('#bot6')
+var box7 = document.querySelector('#bot7')
+var box8 = document.querySelector('#bot8')
+
+var crossCounter = 0
+var circleCounter = 0
+var drawCounter = 0
+// var circle = document.querySelectorAll(".circle")
+// var cross = document.querySelectorAll(".cross")
 
 var boxes = document.querySelectorAll(".boxes")
 var main = document.querySelector('.main')
 var counter = 0
-
 
 //function for player switch
 main.addEventListener('click', function(event) {
@@ -27,16 +27,280 @@ main.addEventListener('click', function(event) {
     } else if (counter % 2 !== 0 && event.target.className == 'boxes') {
         event.target.className = 'circle'
     }
+
+    var circle = document.querySelectorAll(".circle")
+    var cross = document.querySelectorAll(".cross")
+    // circle win
+    if (box0.className === 'circle' && box1.className === 'circle' && box2.className === 'circle') {
+        alert("Circle Win!")
+        for(let i = 0; i <= circle.length; i++) {
+            circle[i].className = 'boxes'
+            cross[i].className = 'boxes'
+            counter = 0
+         }
+    } else if (box3.className === 'circle' && box4.className === 'circle' && box5.className === 'circle') {
+        alert("Circle Win!")
+        for(let i = 0; i <= circle.length; i++) {
+            circle[i].className = 'boxes'
+            cross[i].className = 'boxes'
+            counter = 0
+         }
+    } else if (box6.className === 'circle' && box7.className === 'circle' && box8.className === 'circle') {
+        alert("Circle Win!")
+        for(let i=0; i <= circle.length; i++) {
+            circle[i].className = 'boxes'
+            cross[i].className = 'boxes'
+            counter = 0
+         }
+    // column
+    } else if (box0.className === 'circle' && box3.className === 'circle' && box6.className === 'circle') {
+        alert("Circle Win!")
+        for(let i = 0; i <= circle.length; i++) {
+            circle[i].className = 'boxes'
+            cross[i].className = 'boxes'
+            counter = 0
+         }
+    } else if (box1.className === 'circle' && box4.className === 'circle' && box7.className === 'circle') {
+        alert("Circle Win!")
+        for(let i = 0; i <= circle.length; i++) {
+            circle[i].className = 'boxes'
+            cross[i].className = 'boxes'
+            counter = 0
+         }
+    } else if (box2.className === 'circle' && box5.className === 'circle' && box8.className === 'circle') {
+        alert("Circle Win!")
+        for(let i = 0; i <= circle.length; i++) {
+            circle[i].className = 'boxes'
+            cross[i].className = 'boxes'
+            counter = 0
+         }
+    // diagonal
+    } else if (box0.className === 'circle' && box4.className === 'circle' && box8.className === 'circle') {
+        alert("Circle Win!")
+        for(let i = 0; i <= circle.length; i++) {
+            circle[i].className = 'boxes'
+            cross[i].className = 'boxes'
+            counter = 0
+         }
+    } else if (box6.className === 'circle' && box4.className === 'circle' && box2.className === 'circle') {
+        alert("Circle Win!")
+        for(let i = 0; i <= circle.length; i++) {
+            circle[i].className = 'boxes'
+            cross[i].className = 'boxes'
+            counter = 0
+         }
+    } else if (circle.length === 5) {
+        alert('draw')
+         for(let i = 0; i <= circle.length; i++) {
+            circle[i].className = 'boxes'
+            cross[i].className = 'boxes'
+            counter = 0
+         }
+    } 
+    //cross win
+    if (box0.className === 'cross'&& box1.className === 'cross' && box2.className === 'cross') {
+        alert("Cross Win!")
+        for(let i = 0; i <= circle.length; i++) {
+            circle[i].className = 'boxes'
+            cross[i].className = 'boxes'
+            counter = 0
+         }
+    } else if (box3.className === 'cross' && box4.className === 'cross' && box5.className === 'cross') {
+        alert("Cross Win!")
+        for(let i = 0; i <= circle.length; i++) {
+            circle[i].className = 'boxes'
+            cross[i].className = 'boxes'
+            counter = 0
+         }
+    } else if (box6.className === 'cross' && box7.className === 'cross' && box8.className === 'cross') {
+        alert("Cross Win!")
+        for(let i = 0; i <= circle.length; i++) {
+            circle[i].className = 'boxes'
+            cross[i].className = 'boxes'
+            counter = 0
+         }
+    // column
+    } else if (box0.className === 'cross' && box3.className === 'cross' && box6.className === 'cross') {
+        alert("Cross Win!")
+        for(let i = 0; i <= circle.length; i++) {
+            circle[i].className = 'boxes'
+            cross[i].className = 'boxes'
+            counter = 0
+         }
+    } else if (box1.className === 'cross' && box4.className === 'cross' && box7.className === 'cross') {
+        alert("Cross Win!")
+        for(let i = 0; i <= circle.length; i++) {
+            circle[i].className = 'boxes'
+            cross[i].className = 'boxes'
+            counter = 0
+         }
+    } else if (box2.className === 'cross' && box5.className === 'cross' && box8.className === 'cross') {
+        alert("Cross Win!")
+        for(let i = 0; i <= circle.length; i++) {
+            circle[i].className = 'boxes'
+            cross[i].className = 'boxes'
+            counter = 0
+         }
+    // diagonal
+    } else if (box0.className === 'cross' && box4.className === 'cross' && box8.className === 'cross') {
+        alert("Cross Win!")
+        for(let i = 0; i <= circle.length; i++) {
+            circle[i].className = 'boxes'
+            cross[i].className = 'boxes'
+            counter = 0
+         }
+    } else if (box6.className === 'cross' && box4.className === 'cross' && box2.className === 'cross') {
+        alert("Cross Win!")
+        for(let i = 0; i <= circle.length; i++) {
+            circle[i].className = 'boxes'
+            cross[i].className = 'boxes'
+            counter = 0
+         }
+    } 
+    
 })
 
-//function for validate the winner
-var crossWin = document.querySelectorAll('.cross')
-var circleWin = document.querySelectorAll('.circle')
+
+//reset function
+var resetFunction = document.querySelector('.reset')
+var article = document.querySelector('article')
+
+article.addEventListener('click', function(event) {
+    var circle = document.querySelectorAll(".circle")
+
+    for(let i = 0; i <= circle.length; i++) {
+        circle[i].className = 'boxes'
+        counter = 0
+    }
+})
+
+article.addEventListener('click', function(event) {
+    var cross = document.querySelectorAll(".cross")
+
+    for(let i = 0; i <= cross.length; i++) {
+        cross[i].className = 'boxes'
+        counter = 0
+    }
+})
+
+//reset button sound
 
 
+
+
+
+
+
+
+
+
+
+//used
+//method 1 function for validate the winner
+
+//cross win
+// main.addEventListener('click', function(event) {
     
-    // for (let i = 0; i < winningSituation.length; i++)
+//     // row
+//     if (box0.className === 'cross'&& box1.className === 'cross' && box2.className === 'cross') {
+//         alert("Cross Win!")
+//         crossCounter = crossCounter + 1
+//     } else if (box3.className === 'cross' && box4.className === 'cross' && box5.className === 'cross') {
+//         alert("Cross Win!")
+//         crossCounter = crossCounter + 1
+//     } else if (box6.className === 'cross' && box7.className === 'cross' && box8.className === 'cross') {
+//         alert("Cross Win!")
+//         crossCounter = crossCounter + 1
+//     // column
+//     } else if (box0.className === 'cross' && box3.className === 'cross' && box6.className === 'cross') {
+//         alert("Cross Win!")
+//         crossCounter = crossCounter + 1
+//     } else if (box1.className === 'cross' && box4.className === 'cross' && box7.className === 'cross') {
+//         alert("Cross Win!")
+//         crossCounter = crossCounter + 1
+//     } else if (box2.className === 'cross' && box5.className === 'cross' && box8.className === 'cross') {
+//         alert("Cross Win!")
+//         crossCounter = crossCounter + 1
+//     // diagonal
+//     } else if (box0.className === 'cross' && box4.className === 'cross' && box8.className === 'cross') {
+//         alert("Cross Win!")
+//         crossCounter = crossCounter + 1
+//     } else if (box6.className === 'cross' && box4.className === 'cross' && box2.className === 'cross') {
+//         alert("Cross Win!")
+//         crossCounter = crossCounter + 1
+//     } 
+// })
 
-
-
+//circle win
+// main.addEventListener('click', function(event) {
+//     var circle = document.querySelectorAll(".circle")
+//     var cross = document.querySelectorAll(".cross")
+//     // row
+//     if (box0.className === 'circle' && box1.className === 'circle' && box2.className === 'circle') {
+//         alert("Circle Win!")
+//         for(let i=0; i <= circle.length; i++) {
+//             circle[i].className = 'boxes'
+//             cross[i].className = 'boxes'
+//             counter = 0
+//          }
+//     } else if (box3.className === 'circle' && box4.className === 'circle' && box5.className === 'circle') {
+//         alert("Circle Win!")
+//         for(let i=0; i <= circle.length; i++) {
+//             circle[i].className = 'boxes'
+//             cross[i].className = 'boxes'
+//             counter = 0
+//          }
+//     } else if (box6.className === 'circle' && box7.className === 'circle' && box8.className === 'circle') {
+//         alert("Circle Win!")
+//         for(let i=0; i <= circle.length; i++) {
+//             circle[i].className = 'boxes'
+//             cross[i].className = 'boxes'
+//             counter = 0
+//          }
+//     // column
+//     } else if (box0.className === 'circle' && box3.className === 'circle' && box6.className === 'circle') {
+//         alert("Circle Win!")
+//         for(let i=0; i <= circle.length; i++) {
+//             circle[i].className = 'boxes'
+//             cross[i].className = 'boxes'
+//             counter = 0
+//          }
+//     } else if (box1.className === 'circle' && box4.className === 'circle' && box7.className === 'circle') {
+//         alert("Circle Win!")
+//         for(let i=0; i <= circle.length; i++) {
+//             circle[i].className = 'boxes'
+//             cross[i].className = 'boxes'
+//             counter = 0
+//          }
+//     } else if (box2.className === 'circle' && box5.className === 'circle' && box8.className === 'circle') {
+//         alert("Circle Win!")
+//         for(let i=0; i <= circle.length; i++) {
+//             circle[i].className = 'boxes'
+//             cross[i].className = 'boxes'
+//             counter = 0
+//          }
+//     // diagonal
+//     } else if (box0.className === 'circle' && box4.className === 'circle' && box8.className === 'circle') {
+//         alert("Circle Win!")
+//         for(let i=0; i <= circle.length; i++) {
+//             circle[i].className = 'boxes'
+//             cross[i].className = 'boxes'
+//             counter = 0
+//          }
+//     } else if (box6.className === 'circle' && box4.className === 'circle' && box2.className === 'circle') {
+//         alert("Circle Win!")
+//         for(let i=0; i <= circle.length; i++) {
+//             circle[i].className = 'boxes'
+//             cross[i].className = 'boxes'
+//             counter = 0
+//          }
+//     } else if (circle.length === 5) {
+//         alert('draw')
+//          for(let i=0; i <= circle.length; i++) {
+//             circle[i].className = 'boxes'
+//             cross[i].className = 'boxes'
+//             counter = 0
+//          }
+//     } 
+// })
 
